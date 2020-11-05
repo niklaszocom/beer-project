1370e3ac153cf207588522636ec1653730d3798f
SofiaMEweiss <sofia.weiss@iths.se>
2020-11-05 23:59:55 +0100

    navbar + started with CSS responsive

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const menuIcon = document.querySelector('.hamburger-menu')
+const navbar = document.querySelector('.navbar')
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+})
+
 const url = "https://api.punkapi.com/v2/beers/random";
 
 const button = document.querySelector("#button");
@@ -21,7 +28,7 @@ let displayRandomBeer = (beerName, beerImage, beerId) => {
     imgElement.src = beerImage;
     imgElement.alt = "image:";
 
-    aElement.href = "info.html?name="+beerId;
+    aElement.href = "info.html?name=" + beerId;
     aElement.className = "seeMoreLink";
 
     divElement.appendChild(imgElement);
@@ -39,7 +46,7 @@ let createRandomBeer = (data) => {
     let beerId = data[0].id;
     //console.log(data[0]);
     // Gratisbild: https://pixabay.com/sv/vectors/flaska-%C3%B6l-siluett-svart-dryck-310313/
-    if (data[0].image_url == null) {beerpic = "img/beer_without_image.png";}
+    if (data[0].image_url == null) { beerpic = "img/beer_without_image.png"; }
 
     displayRandomBeer(beer, beerpic, beerId);
 }
@@ -79,5 +86,5 @@ function nysida(param, sidTitel) {
 
     let nyttElement = document.createElement("p");
     eMain.appendChild(nyttElement);
-    nyttElement.textContent=sidTitel;
+    nyttElement.textContent = sidTitel;
 }
