833ad41a23a8ff66591993fc82554b33658d1f66
fredrikpihlgren <70364712+fredrikpihlgren@users.noreply.github.com>
2020-11-05 08:29:49 +0100

    updated js code
    Updated main.js

--- main.js
+++ main.js
@@ -1,45 +1,64 @@
 const url = "https://api.punkapi.com/v2/beers/random";
-const divElement = document.querySelector("div");
-let button = document.querySelector('#button');
 
-const getElement = (beerName, beerImage) => {
+const button = document.querySelector("#button");
+
+
+
+
+let displayRandomBeer = (beerName, beerImage) => {
+
+    const divElement = document.querySelector(".showbeer");
 
     removeAllChildNodes(divElement);
 
-    const sectionElement = document.createElement("section");
-    divElement.appendChild(sectionElement);
-
-    const imgElement = document.createElement("img");
-    const textNode = document.createTextNode(beerName);
     const h2Element = document.createElement("h2");
+    const textNode = document.createTextNode(beerName);
+    const imgElement = document.createElement("img");
 
     imgElement.src = beerImage;
-    sectionElement.appendChild(imgElement);
-    sectionElement.appendChild(h2Element);
-    h2Element.appendChild(textNode);
+    imgElement.alt = "image:";
 
-    divElement.appendChild(sectionElement);
+    divElement.appendChild(h2Element);
+    divElement.appendChild(imgElement);
+    h2Element.appendChild(textNode);
 
 }
 
+
+let createRandomBeer = (data) => {
+    let beer = data[0].name;
+        let beerpic = data[0].image_url;
+        //console.log(data[0]);
+        if (data[0].image_url == null) {beerpic="";}
+
+    displayRandomBeer(beer, beerpic);
+}
+
+
+
 button.addEventListener('click', () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-
-            let beer = data[0].name;
-            let beerpic = data[0].image_url;
-            console.log(data[0]);
-            if (data[0].image_url == null) {beerpic="";}
-
-            getElement(beer, beerpic);
-        })
+    fetcher(url, createRandomBeer);
 });
 
-function removeAllChildNodes(parent) {
 
+
+
+//GENERELLA FUNKTIONER:
+
+//ange den URL som ska hämtas, samt den funktion (utan ()) som skall anropas:
+let fetcher = (url, callback) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        callback(data);
+    })
+
+}
+
+
+//parent skall vara den klass eller det element vars children skall raderas:
+let removeAllChildNodes = (parent) => {
     while (parent.firstChild) {
-
-        parent.removeChild(parent.firstChild);
+        parent.firstChild.remove();
     }
 }
