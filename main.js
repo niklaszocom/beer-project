6825ee3a764e140018e1402d5c0c2048c45b7fcd
fredrikpihlgren <70364712+fredrikpihlgren@users.noreply.github.com>
2020-11-06 08:26:18 +0100

    new code
    added some new code for displaying beer. Doesn't work well yet,

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-const menuIcon = document.querySelector('.hamburger-menu')
-const navbar = document.querySelector('.navbar')
+
+
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
 
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
-})
+});
+
+
+
 
 const url = "https://api.punkapi.com/v2/beers/random";
 
 const button = document.querySelector("#button");
 const eMain = document.querySelector("main");
+
+const noPic="img/beer_without_image.png";
 
 
 
@@ -28,7 +35,12 @@ let displayRandomBeer = (beerName, beerImage, beerId) => {
     imgElement.src = beerImage;
     imgElement.alt = "image:";
 
-    aElement.href = "info.html?name=" + beerId;
+    //aElement.href = "info.html?name=" + beerId;
+    aElement.href = "#";
+    aElement.addEventListener('click', () => {
+        nysida(3, 'Visa öl');
+        showProduct(beerId);
+    });
     aElement.className = "seeMoreLink";
 
     divElement.appendChild(imgElement);
@@ -46,7 +58,7 @@ let createRandomBeer = (data) => {
     let beerId = data[0].id;
     //console.log(data[0]);
     // Gratisbild: https://pixabay.com/sv/vectors/flaska-%C3%B6l-siluett-svart-dryck-310313/
-    if (data[0].image_url == null) { beerpic = "img/beer_without_image.png"; }
+    if (data[0].image_url == null) { beerpic = noPic; }
 
     displayRandomBeer(beer, beerpic, beerId);
 }
@@ -57,6 +69,34 @@ button.addEventListener('click', () => {
     fetcher(url, createRandomBeer);
 });
 
+
+
+//Skapar DOM-element och information för specifik öl-produkt:
+
+function showProduct(beerId) {
+
+    let eContainer = document.createElement("section");
+    eMain.appendChild(eContainer);
+
+    /*
+    let inBlock = [];
+    for (let i=0;i<3;i++) {
+    inBlock[i] = document.createElement("div");
+    eContainer.appendChild(inBlock[i]);
+    inBlock[i].className="testblock";
+    inBlock[i].textContent="container "+i;
+    }
+    */
+
+    let eContainer2 = document.createElement("div");
+    eMain.appendChild(eContainer2);
+    eContainer2.className="testblock";
+    eContainer2.textContent="container2";
+
+    eContainer.className="testblock";
+    eContainer.textContent="container";
+
+}
 
 
 
@@ -83,8 +123,9 @@ let removeAllChildNodes = (parent) => {
 
 function nysida(param, sidTitel) {
     removeAllChildNodes(eMain);
-
+    /*
     let nyttElement = document.createElement("p");
     eMain.appendChild(nyttElement);
     nyttElement.textContent = sidTitel;
+    */
 }
