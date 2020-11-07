0322223a815e18f7866a84e406ce1c53c00d43b3
ritorium <ritorium@users.noreply.github.com>
2020-11-07 19:18:43 +0100

    Random and Search Pages scripts

---
+++ Beer1/Search/search.js
@@ -0,0 +1,121 @@
+const field = document.querySelector("#field");
+const btn = document.querySelector("#btn");
+const main = document.querySelector("main");
+const ul = document.createElement("ol");
+const prevBtn = document.createElement("button");
+const nextBtn = document.createElement("button");
+prevBtn.appendChild(document.createTextNode("prev"));
+nextBtn.appendChild(document.createTextNode("next"));
+// Loader
+const loader = document.createElement("h1");
+loader.appendChild(document.createTextNode("Loading..."));
+// Loader
+let page = 1;
+let displayButtons = false;
+
+const api = "https://api.punkapi.com/v2/";
+
+const response = new XMLHttpRequest();
+
+let currentBeers = []; 
+
+// Поиск
+btn.onclick = (e) => {
+    e.preventDefault();
+    response
+        .open("GET", `${api}beers?page=1&per_page=10&beer_name=${field.value}`, true); 
+    response.send(); 
+    main.appendChild(loader);
+};
+
+response.onreadystatechange = function() {
+    if(response.readyState === 4 && response.status === 200) {
+        main.removeChild(loader);
+        if(!displayButtons) {
+            displayButtons = true;
+            main.appendChild(ul);
+            document.body.appendChild(prevBtn);
+            document.body.appendChild(nextBtn);
+        } else {
+            removeAllChildnode(ul);
+        }
+        page = 1;
+        currentBeers = JSON.parse(response.responseText);
+
+        for(let i = 0; i <= currentBeers.length - 1; i++) {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.href = `/beer/${currentBeers[i].id}`;
+            a.appendChild(document.createTextNode(currentBeers[i].name));
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+    }
+};
+// Поиск
+
+prevBtn.addEventListener("click", () => {
+    if(page > 1) {
+        removeAllChildnode(ul);
+        page--;
+        const response = new XMLHttpRequest();
+
+        response
+            .open("GET", `${api}beers?page=${page}&per_page=10&beer_name=${field.value}`, true);
+
+        response.send(); 
+
+
+        response.onreadystatechange = function() {
+            if(response.readyState === 4 && response.status === 200) {
+                currentBeers = JSON.parse(response.responseText);
+                main.appendChild(ul);
+
+                for(let i = 0; i <= currentBeers.length - 1; i++) {
+                    const li = document.createElement("li");
+                    const a = document.createElement("a");
+                    a.href = `/beer/${currentBeers[i].id}`;
+                    a.appendChild(document.createTextNode(currentBeers[i].name));
+                    li.appendChild(a);
+                    ul.appendChild(li);
+                }
+            }
+        };
+    }
+});
+
+nextBtn.addEventListener("click", () => {
+    if(currentBeers.length === 10) {
+        page++;
+        removeAllChildnode(ul);
+        const response = new XMLHttpRequest();
+
+        response
+            .open("GET", `${api}beers?page=${page}&per_page=10&beer_name=${field.value}`, true);
+
+        response.send(); 
+
+
+        response.onreadystatechange = function() {
+            if(response.readyState === 4 && response.status === 200) {
+                currentBeers = JSON.parse(response.responseText);
+                main.appendChild(ul);
+
+                for(let i = 0; i <= currentBeers.length - 1; i++) {
+                    const li = document.createElement("li");
+                    const a = document.createElement("a");
+                    a.href = `/beer/${currentBeers[i].id}`;
+                    a.appendChild(document.createTextNode(currentBeers[i].name));
+                    li.appendChild(a);
+                    ul.appendChild(li);
+                }
+            }
+        };
+    }
+});
+
+function removeAllChildnode(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
