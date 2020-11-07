b5b858adbe898cb292342363113f5e2e5f92affe
midori1727 <midori.takaichi@iths.se>
2020-11-07 01:04:15 +0100

    html + css

--- search.js
+++ search.js
@@ -1,19 +1,27 @@
+
+
 const api = 'https://api.punkapi.com/v2/beers?per_page=10';
 
 const formElement = document.querySelector("form");
 const mainElement = document.querySelector("main");
+const btnElement = document.querySelector("p")
+
+let array = [];
 
 formElement.addEventListener("submit",onSubmit)
 
 function onSubmit(evt) {
-    const searchStr =  evt.target[0].value;
 
+    const searchStr = evt.target[0].value;
+   
     const url = `${api}&beer_name=${searchStr}`;
 
     getData(url, render);
-
-
+   
     evt.preventDefault();
+
+    array.push(searchStr)
+
 }
 
 
@@ -21,56 +29,106 @@ function getData(url, callback) {
     fetch(url)
     .then(res => res.json())
     .then(data => {
+
         callback(data);
 
     })
     .catch(error => console.log(error));
 }
 
-
 function render(data) {
 
     const ulElement = document.createElement("ul");
 
     ulElement.addEventListener("click", onUlClicked);
+    
 
     for(let i = 0; i < data.length; i ++) {
 
         const beer = data[i];
-
+        
         const liElement = document.createElement("li");  
         liElement.setAttribute("name", beer.id);
         liElement.textContent = beer.name;
-        ulElement.appendChild(liElement)   
-        
+        ulElement.appendChild(liElement)  
+    
     }
+
+
     mainElement.appendChild(ulElement);
-    const btn = document.createElement("button")
-    btn.innerText = "next";
-    mainElement.appendChild(btn);
 
-    let n = 2;
-  
-    btn.addEventListener("click", () => {
+    const prevBtn = document.createElement("button")
+    prevBtn.innerText = "prev";
+    btnElement.appendChild(prevBtn);
 
-        let url = new URL(api);
-        let search_params = url.searchParams;
-        
-        search_params.set('page',n);
-        url.search = search_params.toString();
-        let new_url = url.toString();
-        const  new_urlElement = document.createElement("p");
-        new_urlElement.textContent = new_url;
-        mainElement.appendChild(new_urlElement)
-        n += 1;
-       
-       
-        
-        
-    })
     
 
+    prevBtn.addEventListener("click",() => {
+
+        removeAllChildnode(ulElement)
+
+        let url = `${api}&page=${n}&beer_name=${array[0]}`;
+        n -= 1;
+        // console.log(url)
+
+        fetch(url)
+        .then(res => res.json())
+        .then(nextElement => {
+
+            for(let i = 0; i < nextElement.length; i++) {
+
+                const liElement = document.createElement("li")
+                liElement.setAttribute("name", nextElement[i].id);
+                liElement.textContent = nextElement[i].name;
+                ulElement.appendChild(liElement)
+                mainElement.appendChild(ulElement)
+               
+            }
+     
+        })
+        
+    })
+
+
+    
+    const nextBtn = document.createElement("button")
+    nextBtn.innerText = "next";
+    btnElement.appendChild(nextBtn);
+   
+    let n = 2;
+  
+    nextBtn.addEventListener("click",() => {
+
+        removeAllChildnode(ulElement)
+
+        let url = `${api}&page=${n}&beer_name=${array[0]}`;
+        n += 1;
+        console.log(url)
+
+        fetch(url)
+        .then(res => res.json())
+        .then(nextElement => {
+
+            for(let i = 0; i < nextElement.length; i++) {
+
+                const liElement = document.createElement("li")
+                liElement.setAttribute("name", nextElement[i].id);
+                liElement.textContent = nextElement[i].name;
+                ulElement.appendChild(liElement)
+                mainElement.appendChild(ulElement)
+               
+            }
+     
+        })
+        
+    })
+
+    
+
+    
+   
 }
+
 
 
 function onUlClicked(evt) {
@@ -85,5 +143,3 @@ function removeAllChildnode(parent) {
         parent.removeChild(parent.firstChild)
     }
 }
-
-
