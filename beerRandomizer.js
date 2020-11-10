8e759fe78d15ef9c7328d9d3e0f5727b70728db6
Fredrik Johansson <fredrik.johansson@iths.se>
2020-11-10 16:29:49 +0100

    fixed placeholder for beer info url

--- beerRandomizer.js
+++ beerRandomizer.js
@@ -9,18 +9,20 @@ async function getRandom() {
                 const imageText = document.getElementById('randomName')
                 const image = document.getElementById('randomLabel')
                 const link = document.getElementById('randomLink')
+
                 const imageURL = i.image_url ?? './placeholder.png'
-                
+                const beerURL = './infoPage.html/' + i.id
 
                 const nameElement = document.createElement('p')
                 const nameNode = document.createTextNode(`${i.name}`)
                 nameElement.appendChild(nameNode)
-                
-                
+
+
                 imageText.innerHTML = '';
                 imageText.appendChild(nameElement)
                 image.src = imageURL
 
+                link.href = beerURL
 
             }
             
