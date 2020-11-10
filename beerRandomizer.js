c2a6871bdc974ab6d716696957a34cbf92c1e0c9
Fredrik Johansson <fredrik.johansson@iths.se>
2020-11-10 10:45:43 +0100

    added set img src from get

--- beerRandomizer.js
+++ beerRandomizer.js
@@ -1,17 +1,19 @@
-const axios = require('axios').default;
-
 async function getRandom() {
-
-    axios.get('https://api.punkapi.com/v2/beers/random')
-        .then(function (response) {
-
-            console.log(response);
-        })
-        .catch(function (error) {
-
-            console.log(error);
-        })
-        .then(function () {
-
-        });
+  axios
+    .get('https://api.punkapi.com/v2/beers/random')
+    .then(function (response) {
+      const data = response.data;
+      console.log(data)
+      for (const i of data) {
+        const imgURL = i.image_url
+        console.log(imgURL)
+        document.getElementById("random").src = imgURL
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {});
 }
+
+getRandom();
