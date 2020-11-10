8667cdd71ffe00b8148bcc903c7e74ac74f5a021
Fredrik Johansson <fredrik.johansson@iths.se>
2020-11-10 14:15:01 +0100

    added placeholder for null img

--- beerRandomizer.js
+++ beerRandomizer.js
@@ -1,19 +1,31 @@
 async function getRandom() {
-  axios
-    .get('https://api.punkapi.com/v2/beers/random')
-    .then(function (response) {
-      const data = response.data;
-      console.log(data)
-      for (const i of data) {
-        const imgURL = i.image_url
-        console.log(imgURL)
-        document.getElementById("random").src = imgURL
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {});
+    axios
+        .get('https://api.punkapi.com/v2/beers/random')
+        .then(function (response) {
+            const data = response.data;
+
+            for (const i of data) {
+
+                const imageText = document.getElementsByClassName('beer-name')
+                const image = document.getElementById('random')
+                const imgURL = i.image_url ?? './placeholder.png'
+
+                const textElement = document.createElement('h1')
+                const textNode = document.createTextNode(`${i.name}`)
+                textElement.appendChild(textNode)
+                
+                image.src = imgURL
+                imageText.innerHTML = '';
+                imageText.appendChild(textElement)
+
+
+            }
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {});
 }
 
 getRandom();
